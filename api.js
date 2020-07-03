b048ddc2041ef2ded5f0b91466ae934b9aca1e97
HAKIM <hakim@mbp-de-hakim.home>
2020-07-03 09:38:40 +0200

    first commit

---
+++ api.js
@@ -0,0 +1,85 @@
+// Catching all needed element with DOM
+var capital = document.getElementById("capital");
+var flag = document.querySelector("#flag-container img");
+var population = document.getElementById("population");
+var currencies = document.getElementById("currencies");
+var subregion = document.getElementById("subregion");
+var region = document.getElementById("region");
+var dialingCode = document.getElementById("dialing-code");
+var flagImage = document.getElementById("flagImage");
+var timezone = document.getElementById("timezone");
+var languages = document.getElementById("languages");
+var alpha2Code = document.getElementById("alpha2Code");
+// array used to stock my global object
+var arrayGlobal = [];
+
+
+
+function ajaxGet(url, callback) {
+  //This is how to use XMLHttpRequest to issue HTTP requests in
+  // order to exchange data between the web site and a server.
+  var req = new XMLHttpRequest();
+  req.open("GET", url);
+  req.addEventListener("load", function () {
+    if (req.status >= 200 && req.status < 400) {
+      // Appelle la fonction callback en lui passant la réponse de la requête
+      callback(req.responseText);
+    } else {
+      console.error(req.status + " " + req.statusText + " " + url);
+    }
+  });
+  req.addEventListener("error", function () {
+    console.error("Erreur réseau avec l'URL " + url);
+  });
+  req.send(null);
+}
+ajaxGet("https://restcountries.eu/rest/v2/all", function (response) {
+  var countries = JSON.parse(response);
+  // myArrayOfCountry.push(countries);
+
+  countries.forEach((countrie) => {
+    
+    var myArrayOfCountry = countrie.name;
+    arrayGlobal.push(countrie)
+    
+    
+    
+      // add <option> to <select>
+    var selectList = document.createElement("option");
+    document.getElementById("countries").appendChild(selectList);
+
+    //Create and append the options
+    for (var i = 0; i < myArrayOfCountry.length; i++) {
+      var option = document.createElement("option");
+      option.value = myArrayOfCountry[i];
+      option.text = myArrayOfCountry[i];
+      selectList.appendChild(option);
+    }
+    
+  });
+});
+console.log(arrayGlobal);
+
+
+
+function selectChange() {
+  var selectCountry = document.getElementById("countries").value;
+  // find the input in arrayGlobal
+  const found = arrayGlobal.find(element => element.name == selectCountry);
+
+  // Country Information
+  capital.textContent = found.capital;
+  population.textContent = found.population;
+  currencies.textContent = found.currencies[0].symbol + " - " + found.currencies[0].code + ", " + found.currencies[0].name;
+  subregion.textContent = found.subregion;
+  region.textContent = found.region;
+  dialingCode.textContent = found.callingCodes;
+  timezone.textContent=found.timezones;
+  languages.textContent=found.languages[0].name;
+  alpha2Code.textContent=found.alpha2Code; 
+  // Flag image
+  var flagUrl = found.flag
+  flagImage.setAttribute("src", flagUrl);
+  // document.body.style.backgroundImage = "url('img_tree.png')";
+
+}
